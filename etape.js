51bf33044bf608f7a5dcd4e11f8913f0b95526ee
Kym <e0786366@cmaisonneuve.qc.ca>
2017-03-21 14:42:20 -0400

    etape 1 en cours

--- etape.js
+++ etape.js
@@ -9,5 +9,15 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
 
+app.get('/',function(req,res){
+    res.sendFile(path.normalize(__dirname + 'text/collection_provinces.json'))  
+})
 
 
+MongoClient.connect('mongodb://127.0.0.1:27017/ville-carnet', (err, database) => {
+  if (err) return console.error(err)
+  db = database;
+  app.listen(8081, () => {
+    console.log('connexion à la BD et on écoute sur le port 8081');
+  });
+});
