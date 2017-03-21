9bd27ca887c1f915bde656afd42b008fc9996075
Kym <e0786366@cmaisonneuve.qc.ca>
2017-03-21 14:53:54 -0400

    debut de letape 2 mais toujours pas capable de lire de json tout seul...

--- etape.js
+++ etape.js
@@ -4,20 +4,37 @@ const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const ObjectID = require('mongodb').ObjectID;
 const app = express();
+
+var bdd = "text/collection_provinces.json"
+
 app.set('view engine', 'ejs'); // générateur de template 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
 
-app.get('/',function(req,res){
-    res.sendFile(path.normalize(__dirname + 'text/collection_provinces.json'))  
+
+MongoClient.connect('mongodb://127.0.0.1:27017/ville-carnet', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(8081, () => {
+    console.log('connexion à la BD et on écoute sur le port 8081')
+  })
+})
+
+app.get('/',  (req, res) => {
+   console.log('la route route get / = ' + req.url)
+ 
+    var cursor = db.collection('adresse').find().toArray(function(err, resultat){
+       if (err) return console.log(err)
+    // renders index.ejs
+    // affiche le contenu de la BD
+    res.render('index.ejs', {adresse: resultat})
+
+    }) 
 })
 
 
-MongoClient.connect('mongodb://127.0.0.1:27017/ville-carnet', (err, database) => {
-  if (err) return console.error(err)
-  db = database;
-  app.listen(8081, () => {
-    console.log('connexion à la BD et on écoute sur le port 8081');
-  });
-});
+app.get('/formulaire',  (req, res) => {
+   console.log('la route  get / = ' + req.url)
+   res.sendFile(__dirname + "index.ejs")
+})
