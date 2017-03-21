b81a37c53120b296864bd514374776522d044e03
Kym <e0786366@cmaisonneuve.qc.ca>
2017-03-21 17:42:02 -0400

    affiche enfin le tableau, ce fut fastidieux

--- etape.js
+++ etape.js
@@ -9,9 +9,9 @@ app.set('view engine', 'ejs'); // générateur de template
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
+var db // variable qui contiendra le lien sur la BD
 
-
-MongoClient.connect('mongodb://127.0.0.1:27017/carnet', (err, database) => {
+MongoClient.connect('mongodb://127.0.0.1/carnet', (err, database) => {
   if (err) return console.log(err)
   db = database
   app.listen(8081, () => {
@@ -19,18 +19,21 @@ MongoClient.connect('mongodb://127.0.0.1:27017/carnet', (err, database) => {
   })
 })
 
-//pour afficher le fichier index.ejs
-app.get('/',  (req, res) => {
-	res.render("index.ejs");
-
+// collection yeah
+app.get('/',  (req, res, next) => {
+    var cursor = db.collection('adresse').find().toArray(function(err, resultat){
+    if(err) return next(err);
+		res.render('index.ejs', {adresse: res});
+	});
 })
 
-var server = app.listen(8080, function () {
+/*var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
 
    console.log("L'application écoute sur http://%s", host, port)
 })
+*/
 
 //etape #1
 app.get('/fichier', function (req, res) {
@@ -42,19 +45,21 @@ app.get('/fichier', function (req, res) {
 })
 
 //etape #2
-app.get('/provinces', function (req, res) {
+app.get('/provinces',  (req, res, next) => {
+	var obj;
 	fs.readFile( __dirname + "/public/text/" + "collection_provinces.json", 'utf8', function (err, data) {
-		if(err) return next(err);
-		console.log( data );
-		res.render('index.ejs', {provinces : JSON.parse(data)})
+		if(err) return console.error(err);
+		obj = JSON.parse(data);
+		console.log(obj);
+		res.render('index.ejs', {adresse: obj});
 	});
-})
+});
 
 //etape #3
 
 //etape #4
 /*
-db.collection('province').insertOne(newRecord, (err, resultat) => {
+db.collection('carnet').insertOne(newRecord, (err, resultat) => {
     if(err) return next(err);
     res.send({ "id": resultat.insertedId });
   }); 
